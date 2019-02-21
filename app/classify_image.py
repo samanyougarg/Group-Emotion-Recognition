d852fedf037f3378d7189043eeb981f31740a0b8
Samanyou Garg <samanyugarg@gmail.com>
2019-02-21 08:46:17 +0000

    राधे राधे: Label detection fixes

--- app/classify_image.py
+++ app/classify_image.py
@@ -6,11 +6,30 @@ from . import image_preprocessing
 from . import cnn
 from . import bayesian_network
 
+image_label_dict = {
+    "image_happy_1.jpg": ['People', 'Friendship', 'Fun', 'Event', 'Drinking', 'Happy', 'Picnic', 'Recreation', 'Smile', 'Leisure'],
+    "image_happy_2.jpg": ['Hair', 'Facial expression', 'Fun', 'Friendship', 'Hairstyle', 'Smile', 'Yellow', 'Event', 'Human', 'Laugh'],
+    "image_happy_neutral_1.jpg": ['People', 'Family taking photos together', 'Social group', 'Child', 'Father', 'Event','Family', 'Fun', 'Smile', 'Photography'],
+    "image_happy_neutral_2.jpg": ['People', 'Social group', 'Fun', 'Team', 'Event', 'Crew', 'Tourism', 'Uniform', 'Leisure', 'Smile'],
+    "image_happy_neutral_3.jpg": ['People', 'Tribe', 'Fun', 'Human', 'Smile', 'Community', 'Child', 'Happy', 'Adaptation'],
+    "image_happy_neutral_4.jpg": ['People', 'Child', 'Smile', 'Community', 'Youth', 'Friendship', 'Adaptation', 'Fun', 'Happy', 'Event'],
+    "image_neutral_sad.jpg": ['Hair', 'Face', 'Chin', 'Hairstyle', 'Cool', 'Forehead', 'Black hair', 'Fun', 'Neck', 'Smile'],
+    "image_neutral.jpg": ['Face', 'People', 'Facial expression', 'Child', 'Smile', 'Skin', 'Fun', 'Child model','Human', 'Happy'],
+    "image_sad_1.jpg": ['People', 'Community', 'Tribe', 'Adaptation', 'Tradition', 'Event', 'Child', 'Smile', 'Tourism', 'Turban']
+}
+
 # function to classify an image
 def classify_image(image_path, cnn_model, bayesian_model, labels_list):
-    # get the labels for the image from Google Vision API
-    # labels = image_preprocessing.detect_labels(image_path)
-    labels = ["people", "tribe", "leisure", "fun", "temple", "vacation", "tradition", "happiness", "tourism", "travel"]
+    # get the image name from the image path
+    image_name = image_path.split('/')[-1]
+    labels = []
+
+    # if image is from collection, get the labels from the dictionaru
+    if image_name in image_label_dict.keys():
+        labels = image_label_dict[image_name]
+    # else get the labels from the Google Vision API
+    else:
+        labels = image_preprocessing.detect_labels(image_path)
 
     print("RadhaKrishna")
     print(labels)
